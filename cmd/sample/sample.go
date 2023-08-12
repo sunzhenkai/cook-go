9d3f73a57aa99d971bdd9f769ecc5730e145f225
zhenkai.sun <zhenkai.sun@qq.com>
2023-08-12 09:34:58 +0800

    mod

--- cmd/sample/sample.go
+++ cmd/sample/sample.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"math/rand"
+	"os"
+)
+
+var checkLog = logrus.New()
+
+type ISimpleFormatter struct {
+}
+
+func (f *ISimpleFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	return append([]byte(entry.Message), '\n'), nil
+}
+
+func InitCheckLog() {
+	file, err := os.OpenFile("tmp.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	checkLog.SetFormatter(new(ISimpleFormatter))
+	if err == nil {
+		checkLog.Out = file
+	}
+}
+
 func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(rand.Intn(100))
+	}
+	InitCheckLog()
+	m := make(map[string]interface{})
+	m["a"] = 1
+	m["b"] = "c"
+	bs, _ := json.Marshal(m)
+	fmt.Println(string(bs))
+	checkLog.Println(string(bs))
 	SampleA()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module cooking-go
 
 go 1.15
 
-require google.golang.org/protobuf v1.26.0
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	google.golang.org/protobuf v1.26.0
+)
